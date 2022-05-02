3273cffa67a9b5d5cb3bd8823f0d387a5b13d4b9
laurentravix <laurentravix@gmail.com>
2022-05-02 08:27:59 +0200

    feat: effet scrolling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import NavBar from "./NavBar/NavBar";
 import Container from "react-bootstrap/Container";
 import "../Header/header.css"
 
 const Header = () => {
+
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 56);
+    });
+  }, []); 
+
+
   return (
-    <div className="header">
+    <div className={`header ${scroll ? "sticky" : ""}`}>
       <Container>
         <NavBar />
       </Container>
