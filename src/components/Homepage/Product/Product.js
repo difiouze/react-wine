991c1e937ccaa082a05b32ba9bdf5908960f9383
laurentravix <laurentravix@gmail.com>
2022-05-16 20:10:24 +0200

    fix: destructuring bottles instead of props

--- src/components/Homepage/Product/Product.js
+++ src/components/Homepage/Product/Product.js
@@ -2,25 +2,25 @@ import React from "react";
 import Button from 'react-bootstrap/Button'
 import ProductModal from "../ProductModal/ProductModal";
 
-const Product = (props) => {
+const Product = ( {bottle} ) => {
   const [modalShow, setModalShow] = React.useState(false);
 
   return (
     <div className="card">
-      <img src={props.bottle.image} alt={props.bottle.name} />
-      <h3>{props.bottle.name}</h3>
-      <span>{props.bottle.description}</span>
-      <span>Price: {props.bottle.price}$</span>
+      <img src={bottle.image} alt={bottle.name} />
+      <h3>{bottle.name}</h3>
+      <span>{bottle.description}</span>
+      <span>Price: {bottle.price}$</span>
       <>
       <Button variant="primary" onClick={() => setModalShow(true)}>
         En savoir plus
       </Button>
 
       <ProductModal
-        name={props.bottle.name}
-        image={props.bottle.image}
-        description={props.bottle.description}
-        price={props.bottle.price}
+        name={bottle.name}
+        image={bottle.image}
+        description={bottle.description}
+        price={bottle.price}
         show={modalShow}
         onHide={() => setModalShow(false)}
       />
