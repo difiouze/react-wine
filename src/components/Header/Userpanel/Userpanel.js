6886949689b202babfa95f804195c122a7ef4264
laurentravix <laurentravix@gmail.com>
2022-05-20 10:55:45 +0200

    Nav bar quantity

--- src/components/Header/Userpanel/Userpanel.js
+++ src/components/Header/Userpanel/Userpanel.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CartContext from '../../../context/CartContext'
 import { Link } from "react-router-dom"
 
@@ -6,11 +6,18 @@ import { Link } from "react-router-dom"
 const Userpanel = () => {
 
   const { cart } = useContext(CartContext);
+  const [quantity, setQuantity] = useState([0]);
+
+useEffect(() => {
+  let all = cart.map(e => e.qty);
+  setQuantity(all.reduce((a,b) => a + b, 0))
+
+}, [cart])
 
   return (
     <div>
         <Link to='/'>Login</Link>
-        <Link to='/checkout'>{cart.length}</Link>
+        <Link to='/checkout'>{quantity}</Link>
     </div>
   )
 }
