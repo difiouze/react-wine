742327cfe584893172b7cad84875f07620a25243
laurentravix <laurentravix@gmail.com>
2022-05-27 15:46:03 +0200

    mise en page modal

--- src/components/Homepage/ProductModal/ProductModal.js
+++ src/components/Homepage/ProductModal/ProductModal.js
@@ -3,6 +3,7 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import "./productmodal.css";
 import CartContext from "../../../context/CartContext";
+import { Link } from "react-router-dom";
 
 const ProductModal = ({
   name,
@@ -22,23 +23,33 @@ const ProductModal = ({
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">{name}</Modal.Title>
-      </Modal.Header>
+      <Modal.Header closeButton></Modal.Header>
       <Modal.Body>
-        <div className="card">
-          <img src={image} alt={name} />
-          <h3>{name}</h3>
-          <span>{description}</span>
-          <span>Price: {price}$</span>
+        <div className="card product-modal">
+          <div className="product-modal__left">
+            <img className="product-modal__img" src={image} alt={name} />
+          </div>
+          <div className="product-modal__right">
+            <span className="product-modal__name">{name}</span>
+            <p className="product-modal__description">{description}</p>
+            <span className="product-modal__price">Prix: {price}€</span>
+            <Button
+              className="product-modal__btn"
+              onClick={() => addToCart(name, image, description, price, id)}
+            >
+              Ajouter au panier
+            </Button>
+          </div>
         </div>
       </Modal.Body>
 
-      <Modal.Footer>
-        <Button onClick={() => addToCart(name, image, description, price, id)}>
-          Ajouter au panier
-        </Button>
-        <Button onClick={onHide}>Fermer</Button>
+      <Modal.Footer className="product-modal__footer">
+        <Link to="/">
+          <button className="btn btn-secondary product-modal__back">Continuer mes achats</button>
+        </Link>
+        <Link to="/checkout">
+          <button className="btn btn-primary product-modal__checkout">Régler mes achats &gt;</button>
+        </Link>
       </Modal.Footer>
     </Modal>
   );
